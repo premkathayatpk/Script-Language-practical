0a4a2ed59d3eaa29a1452fe8cf0f93908964cae5
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-30 17:36:58 +0545

    add 1php

---
+++ SL Lab 2/uploads/3index.php
@@ -0,0 +1,14 @@
+<!-- index.php -->
+<?php
+    // Include header file
+    include 'header.php';
+?>
+
+<main>
+    <h2>Welcome to Our Website</h2>
+</main>
+
+<?php
+    // Require footer file
+    require 'footer.php';
+?>
