6a8bcca58e3f00e46bc55c56cfa83f84667ff24c
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-01 14:07:26 +0545

    add header.php

---
+++ SL Lab 2/uploads/header.php
@@ -0,0 +1,18 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Include and Require Example</title>
+</head>
+<body>
+    <header>
+        <h1>Header </h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
