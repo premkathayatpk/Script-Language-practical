8ddbd945ae8fb21f365eb0566a10535608e5a399
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-03 21:06:09 +0545

    add get_val method

--- SL Lab3/objectClass.php
+++ SL Lab3/objectClass.php
@@ -3,10 +3,19 @@ class student{
     public $name;
     public $address;
     public $roll;
-}
+
 function set_name($name){
     this->$name = $name;
 }
 function set_address($address){
     $this-> $address = $address;
+}
+function set_roll($roll){
+    $this->$roll = $roll;
+}
+function get_val(){
+    echo "Name : ". $this -> name ."<br>";  
+    echo "Address : ". $this -> address . "<br>";
+    echo "Roll No. : ". $this -> roll . "<br><hr>";
+    }
 }
