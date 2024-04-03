4d3791a54af3fb4b94e0efb9022b6905b4ebb999
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-03 20:58:40 +0545

    add stud class

--- SL Lab3/objectClass.php
+++ SL Lab3/objectClass.php
@@ -1,33 +1,6 @@
 <?php
-
-// Define a class called Person
-class Person {
-    // Properties
+class student{
     public $name;
-    public $age;
-    
-    // Constructor method
-    public function __construct($name, $age) {
-        $this->name = $name;
-        $this->age = $age;
-    }
-    
-    // Method to display information about the person
-    public function displayInfo() {
-        echo "Name: " . $this->name . "<br>";
-        echo "Age: " . $this->age . "<br>";
-    }
+    public $address;
+    public $roll;
 }
-
-// Create objects of the Person class
-$person1 = new Person("Ram", 30);
-$person2 = new Person("Sita", 25);
-
-// Display information about the persons
-echo "Person 1:<br>";
-$person1->displayInfo();
-echo "<br>";
-
-echo "Person 2:<br>";
-$person2->displayInfo();
-?>
