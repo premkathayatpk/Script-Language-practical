0b99e2fc66983f15a12c635ca75e13022059c20a
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-06 12:16:16 +0000

    add class

--- SL Lab3/polymorphism.php
+++ SL Lab3/polymorphism.php
@@ -2,3 +2,14 @@
 interface Shape {
     public function getArea();
 }
+class Circle implements Shape {
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function getArea() {
+        return pi() * pow($this->radius, 2);
+    }
+}
