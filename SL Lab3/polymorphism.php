4a1303cd5e3b49f7281d55d91b51197a0384c048
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-04-06 12:16:49 +0000

    add override

--- SL Lab3/polymorphism.php
+++ SL Lab3/polymorphism.php
@@ -13,3 +13,16 @@ class Circle implements Shape {
         return pi() * pow($this->radius, 2);
     }
 }
+class Rectangle implements Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getArea() {
+        return $this->width * $this->height;
+    }
+}
